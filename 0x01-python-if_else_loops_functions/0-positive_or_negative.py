cc70aded8a1810d7ad33791f46af3fde41a58d42
Cnjerii <christinenjeri162@gmail.com>
2022-08-23 03:46:51 -0700

    python

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -7,12 +7,12 @@ def zero_positive_or_negative(n):
     """
     prints if a number is positive, negative or zero
 """
-if n == 0:
-    return f"{n:d} is zero"
-elif n > 1:
-    return f"{n:d} is positive"
-else:
-    return f"{n:d} is negative"
+    if n == 0:
+        return f"{n:d} is zero"
+    elif n > 1:
+        return f"{n:d} is positive"
+    else:
+        return f"{n:d} is negative"
 
 if __name__ == "__main__":
     print(zero_positive_or_negative(number))
