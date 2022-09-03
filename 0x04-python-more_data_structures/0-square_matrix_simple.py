ce2f7f6de5b52f94b110abad3d2386eba6a2ed63
Cnjerii <christinenjeri162@gmail.com>
2022-09-03 02:58:02 -0700

    python

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+square_matrix_simple(matrix=[]):
+    matrix = [
+            [1, 2],
+            [3, 4],
+            ]
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
