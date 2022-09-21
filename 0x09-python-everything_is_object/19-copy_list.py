affb64beaeef25262cfaee83b22ddd37a6d6e4dd
Cnjerii <christinenjeri162@gmail.com>
2022-09-21 07:20:23 -0700

    object

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,12 +1,3 @@
 my_list = [1, 2, 3]
-print(my_list)
-
-	new_list = my_list.copy()
-	prinr(my_list)
-	print(new_list)
-
-	print(new_list == my_list)
-	print(new_list is my_list)
-
-	print(id(my_list[0]))
-	print(id(new_list[0]))
+new_list = my_list.copy()
+print('Copied List:', new_list)
